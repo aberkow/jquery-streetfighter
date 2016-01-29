418f66e2dabc41ef160469c64d8900a4cdcff335
aberkow <berkowitz.clarinet@gmail.com>
2016-01-29 11:34:21 -0500

    animated haduken

--- js/app.js
+++ js/app.js
@@ -2,22 +2,33 @@ $(document).ready(function() {
     $('.ryu').mouseenter(function() {
         $('.ryu-still').hide();
         $('.ryu-ready').show();
+        
     })
     $('.ryu').mouseleave(function() {
         $('.ryu-still').show();
         $('.ryu-ready').hide();
     })
+    
     $('.ryu').mousedown(function() {
+        //fire haduken and play haduken sound
         console.log('mouse down');
         $('.ryu-ready').hide();
         $('.ryu-throwing').show();
-        
-        //fire haduken and play haduken sound
+        $('.haduken').show().animate(
+            {'left': '1020px'},
+            500,
+            function() {
+                $(this).hide();
+                $(this).css('left', '520px');
+            }
+        );
     })
     $('.ryu').mouseup(function() {
-        console.log('mouse up');
         //return to still position
+        console.log('mouse up'); 
         $('.ryu-throwing').hide();
         $('.ryu-ready').show();
+        
     });
+  
 });
